9b0c2ac1abbf9648fd528fabf3cd5a4ed5fe60ef
Shane Vitarana <shanev@gmail.com>
2022-09-04 23:28:08 -0400

    Renamed to sg-dao and removed unused minter

--- contracts/dao-minter/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod contract;
-mod error;
-pub mod msg;
-pub mod state;
-pub use crate::error::ContractError;
-pub mod helpers;
-
-#[cfg(test)]
-mod integration_tests;
--- contracts/sg-dao/examples/schema.rs
+++ contracts/sg-dao/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw3_nft_dao::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use sg_dao::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
