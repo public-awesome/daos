a526575cb1fbb68bfebd5b7e15431272d7f48240
Shane Vitarana <shanev@gmail.com>
2022-11-06 16:49:14 +0000

    Remove unused tests

--- contracts/sg-nft-group/src/contract.rs
+++ contracts/sg-nft-group/src/contract.rs
@@ -252,20 +252,9 @@ fn list_members(
 #[cfg(test)]
 mod tests {
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{
-        coin, from_slice, CosmosMsg, OverflowError, OverflowOperation, StdError, Storage,
-    };
-    use cw4::{member_key, TOTAL_KEY};
-    use cw_utils::Duration;
-
-    use crate::error::ContractError;
 
     use super::*;
 
-    const INIT_ADMIN: &str = "juan";
-    const USER1: &str = "somebody";
-    const USER2: &str = "else";
-    const USER3: &str = "funny";
     const CW721_ADDRESS: &str = "wasm1234567890";
 
     fn default_instantiate(deps: DepsMut) {
@@ -280,21 +269,6 @@ mod tests {
         instantiate(deps, mock_env(), info, msg).unwrap();
     }
 
-    // fn unbond(mut deps: DepsMut, user1: u128, user2: u128, user3: u128, height_delta: u64) {
-    //     let mut env = mock_env();
-    //     env.block.height += height_delta;
-
-    //     for (addr, stake) in &[(USER1, user1), (USER2, user2), (USER3, user3)] {
-    //         if *stake != 0 {
-    //             let msg = ExecuteMsg::Exit {
-    //                 tokens: Uint128::new(*stake),
-    //             };
-    //             let info = mock_info(addr, &[]);
-    //             execute(deps.branch(), env.clone(), info, msg).unwrap();
-    //         }
-    //     }
-    // }
-
     #[test]
     fn proper_instantiation() {
         let mut deps = mock_dependencies();
@@ -303,146 +277,4 @@ mod tests {
         let res = query_total_weight(deps.as_ref()).unwrap();
         assert_eq!(0, res.weight);
     }
-
-    fn get_member(deps: Deps, addr: String, at_height: Option<u64>) -> Option<u64> {
-        let raw = query(deps, mock_env(), QueryMsg::Member { addr, at_height }).unwrap();
-        let res: MemberResponse = from_slice(&raw).unwrap();
-        res.weight
-    }
-
-    // this tests the member queries
-    fn assert_users(
-        deps: Deps,
-        user1_weight: Option<u64>,
-        user2_weight: Option<u64>,
-        user3_weight: Option<u64>,
-        height: Option<u64>,
-    ) {
-        let member1 = get_member(deps, USER1.into(), height);
-        assert_eq!(member1, user1_weight);
-
-        let member2 = get_member(deps, USER2.into(), height);
-        assert_eq!(member2, user2_weight);
-
-        let member3 = get_member(deps, USER3.into(), height);
-        assert_eq!(member3, user3_weight);
-
-        // this is only valid if we are not doing a historical query
-        if height.is_none() {
-            // compute expected metrics
-            let weights = vec![user1_weight, user2_weight, user3_weight];
-            let sum: u64 = weights.iter().map(|x| x.unwrap_or_default()).sum();
-            let count = weights.iter().filter(|x| x.is_some()).count();
-
-            // TODO: more detailed compare?
-            let msg = QueryMsg::ListMembers {
-                start_after: None,
-                limit: None,
-            };
-            let raw = query(deps, mock_env(), msg).unwrap();
-            let members: MemberListResponse = from_slice(&raw).unwrap();
-            assert_eq!(count, members.members.len());
-
-            let raw = query(deps, mock_env(), QueryMsg::TotalWeight {}).unwrap();
-            let total: TotalWeightResponse = from_slice(&raw).unwrap();
-            assert_eq!(sum, total.weight); // 17 - 11 + 15 = 21
-        }
-    }
-
-    // #[test]
-    // fn bond_stake_adds_membership() {
-    //     let mut deps = mock_dependencies();
-    //     default_instantiate(deps.as_mut());
-    //     let height = mock_env().block.height;
-
-    //     // Assert original weights
-    //     assert_users(deps.as_ref(), None, None, None, None);
-
-    //     // ensure it rounds down, and respects cut-off
-    //     bond(deps.as_mut(), 12_000, 7_500, 4_000, 1);
-
-    //     // Assert updated weights
-    //     assert_stake(deps.as_ref(), 12_000, 7_500, 4_000);
-    //     assert_users(deps.as_ref(), Some(12), Some(7), None, None);
-
-    //     // add some more, ensure the sum is properly respected (7.5 + 7.6 = 15 not 14)
-    //     bond(deps.as_mut(), 0, 7_600, 1_200, 2);
-
-    //     // Assert updated weights
-    //     assert_stake(deps.as_ref(), 12_000, 15_100, 5_200);
-    //     assert_users(deps.as_ref(), Some(12), Some(15), Some(5), None);
-
-    //     // check historical queries all work
-    //     assert_users(deps.as_ref(), None, None, None, Some(height + 1)); // before first stake
-    //     assert_users(deps.as_ref(), Some(12), Some(7), None, Some(height + 2)); // after first stake
-    //     assert_users(deps.as_ref(), Some(12), Some(15), Some(5), Some(height + 3));
-    //     // after second stake
-    // }
-
-    // #[test]
-    // fn unbond_stake_update_membership() {
-    //     let mut deps = mock_dependencies();
-    //     default_instantiate(deps.as_mut());
-    //     let height = mock_env().block.height;
-
-    //     // ensure it rounds down, and respects cut-off
-    //     bond(deps.as_mut(), 12_000, 7_500, 4_000, 1);
-    //     unbond(deps.as_mut(), 4_500, 2_600, 1_111, 2);
-
-    //     // Assert updated weights
-    //     assert_stake(deps.as_ref(), 7_500, 4_900, 2_889);
-    //     assert_users(deps.as_ref(), Some(7), None, None, None);
-
-    //     // Adding a little more returns weight
-    //     bond(deps.as_mut(), 600, 100, 2_222, 3);
-
-    //     // Assert updated weights
-    //     assert_users(deps.as_ref(), Some(8), Some(5), Some(5), None);
-
-    //     // check historical queries all work
-    //     assert_users(deps.as_ref(), None, None, None, Some(height + 1)); // before first stake
-    //     assert_users(deps.as_ref(), Some(12), Some(7), None, Some(height + 2)); // after first bond
-    //     assert_users(deps.as_ref(), Some(7), None, None, Some(height + 3)); // after first unbond
-    //     assert_users(deps.as_ref(), Some(8), Some(5), Some(5), Some(height + 4)); // after second bond
-
-    //     // error if try to unbond more than stake (USER2 has 5000 staked)
-    //     let msg = ExecuteMsg::Exit {
-    //         tokens: Uint128::new(5100),
-    //     };
-    //     let mut env = mock_env();
-    //     env.block.height += 5;
-    //     let info = mock_info(USER2, &[]);
-    //     let err = execute(deps.as_mut(), env, info, msg).unwrap_err();
-    //     assert_eq!(
-    //         err,
-    //         ContractError::Std(StdError::overflow(OverflowError::new(
-    //             OverflowOperation::Sub,
-    //             5000,
-    //             5100
-    //         )))
-    //     );
-    // }
-
-    // #[test]
-    // fn raw_queries_work() {
-    //     // add will over-write and remove have no effect
-    //     let mut deps = mock_dependencies();
-    //     default_instantiate(deps.as_mut());
-    //     // Set values as (11, 6, None)
-    //     bond(deps.as_mut(), 11_000, 6_000, 0, 1);
-
-    //     // get total from raw key
-    //     let total_raw = deps.storage.get(TOTAL_KEY.as_bytes()).unwrap();
-    //     let total: u64 = from_slice(&total_raw).unwrap();
-    //     assert_eq!(17, total);
-
-    //     // get member votes from raw key
-    //     let member2_raw = deps.storage.get(&member_key(USER2)).unwrap();
-    //     let member2: u64 = from_slice(&member2_raw).unwrap();
-    //     assert_eq!(6, member2);
-
-    //     // and execute misses
-    //     let member3_raw = deps.storage.get(&member_key(USER3));
-    //     assert_eq!(None, member3_raw);
-    // }
 }
