f355b9ac1c8a92e4004c226aa07199f86ff1f65b
Shane Vitarana <shanev@gmail.com>
2022-11-15 15:33:35 +0000

    Add nft group tests

--- contracts/sg-gov/src/contract.rs
+++ contracts/sg-gov/src/contract.rs
@@ -272,6 +272,8 @@ pub fn reply(deps: DepsMut, _env: Env, msg: Reply) -> Result<Response, ContractE
                     }
                 })?);
 
+            // println!("sg-nft-group: {}", res.contract_address);
+
             let total_weight = group.total_weight(&deps.querier)?;
             let config = CONFIG.load(deps.storage)?;
             config.threshold.validate(total_weight)?;
--- contracts/sg-gov/src/lib.rs
+++ contracts/sg-gov/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod contract;
 mod error;
-pub mod integration_tests;
+pub mod integration_group_tests;
+pub mod integration_nft_group_tests;
 pub mod msg;
 pub mod state;
 
